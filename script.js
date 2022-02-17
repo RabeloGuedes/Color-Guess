ed5a34c2dcdb4daf1ee3be5802e98991dc6bf62b
RabeloGuedes <krepscredian@gmail.com>
2022-02-17 18:05:16 -0300

    feat && style: Adiciona seis divs no html, cria lógica de randomização de cores a cada renderização e estiliza as divs para se tornarem redondas e padronizadas.

--- script.js
+++ script.js
@@ -6,15 +6,25 @@ function randomColor() {
   return color;
 }
 let corRgb = document.querySelector('#rgb-color');
-let resetBtn = document.querySelector('#resetBtn');
+let resetBtn = document.querySelector('#reset-btn');
+let circles = document.querySelectorAll('.ball');
 
 resetBtn.addEventListener('click',function () {
   let color = randomColor();
   corRgb.innerText = `(${color[0]}, ${color[1]}, ${color[2]})`;
+  circleColorGenerator();
 })
+
+function circleColorGenerator() {
+  for (let index = 0; index < circles.length; index += 1) {
+    color = randomColor();
+    circles[index].style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+  }
+}
 
 window.onload = function () {
   let color = randomColor();
   corRgb.innerText = `(${color[0]}, ${color[1]}, ${color[2]})`;
+  circleColorGenerator();
 }
 
